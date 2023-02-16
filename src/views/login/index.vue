ddfbf3d71a52b9d642b7672daecacde93e990bbf
wwgweb <827104236@qq.com>
2023-02-16 16:12:53 +0800

    初始化登陆页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field v-model="user.mobile" name="手机号" placeholder="请输入手机号">
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field v-model="user.code" name="验证码" placeholder="请输入验证码">
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      user: {
+        mobile: '1111111111', // 手机号
+        code: '1111' // 验证码
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    async onSubmit () {
+      // 1. 展示登陆中 loading
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0 // 持续时间，默认 2000，0 表示持续展示不关闭
+      })
+
+      // 2. 请求登录
+      try {
+        const { data } = await login(this.user)
+        this.$store.commit('setUser', data.data)
+        this.$toast.success('登录成功')
+
+        // 登录成功，跳转回原来页面
+        // back 的方式不严谨，后面讲功能优化的时候再说
+        this.$router.back()
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail('手机号或验证码错误')
+        } else {
+          this.$toast.fail('登录失败，请稍后重试')
+        }
+      }
+    }
+
+    // 4. 根据请求响应结果处理后续操作
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
